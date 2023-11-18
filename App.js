35ae76f1b04243faf90269bf7d99041551e3a423
04Andra <andra.malaescu04@e-uvt.ro>
2023-11-18 09:02:44 +0200

    register screen

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import {StyleSheet, Text, View} from 'react-native';
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
-import {NavigationContainer, useIsFocused} from "@react-navigation/native";
+import {NavigationContainer} from "@react-navigation/native";
 import Login from "./libs/screens/auth/Login";
 import Register from "./libs/screens/auth/Register";
 import Home from "./libs/screens/app/Home";
@@ -11,12 +11,15 @@ import Profile from "./libs/screens/app/Profile";
 import {faHome as fasHome} from '@fortawesome/free-solid-svg-icons/faHome';
 import {faPenToSquare as fasPenToSquare} from '@fortawesome/free-solid-svg-icons/faPenToSquare'
 import {faUserCircle as fasUserCircle} from '@fortawesome/free-solid-svg-icons/faUserCircle'
+import {faEnvelope as fasEnvelope} from '@fortawesome/free-solid-svg-icons/faEnvelope';
+import {faUser as fasUser} from '@fortawesome/free-solid-svg-icons/faUser';
+import {faLock as fasLock} from '@fortawesome/free-solid-svg-icons/faLock';
 import {FontAwesomeIcon} from "@fortawesome/react-native-fontawesome";
 import {library} from "@fortawesome/fontawesome-svg-core";
 import designColors from "./constants/Colors";
 
 library.add(
-    fasHome, fasPenToSquare, fasUserCircle
+    fasHome, fasPenToSquare, fasUserCircle, fasEnvelope, fasUser, fasLock
 );
 
 const Stack = createNativeStackNavigator();
@@ -25,15 +28,13 @@ const Tab = createBottomTabNavigator();
 const AuthStack = () => {
     return (
         <Stack.Navigator>
-            <Stack.Screen options={{headerShown: false}} name={"Login"} component={Login}/>
             <Stack.Screen options={{headerShown: false}} name={"Register"} component={Register}/>
+            <Stack.Screen options={{headerShown: false}} name={"Login"} component={Login}/>
         </Stack.Navigator>
     )
 }
 
-
 const AppStack = () => {
-
     return (
         <Tab.Navigator  screenOptions={screenOptions}>
 
@@ -66,7 +67,7 @@ export default function App() {
     return (
         <NavigationContainer>
             {
-                true ? AppStack() : AuthStack()
+                false ? AppStack() : AuthStack()
             }
         </NavigationContainer>
     );
